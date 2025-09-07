2eee2afeba22ee5ca8eae71d9aa1d5ee816f4ad4
Tricon2zElf <158119828+tricon2elf@users.noreply.github.com>
2025-09-08 08:00:38 +1200

    Changed AuthServer to use Client.SendAsync and simplified the main switch case

--- AISpace.Auth.Server/AuthServer.cs
+++ AISpace.Auth.Server/AuthServer.cs
@@ -36,66 +36,48 @@ internal class AuthServer
         {
             ClientContext Client = packet.Client;
             string ClientID = packet.Client.Id.ToString();
-            byte[] response = new byte[5];
             var payload = packet.Data;
             switch ((PacketType)packet.Type)
             {
                 case PacketType.VersionCheckRequest:
-                    {
-                        var req = VersionCheckRequest.FromBytes(payload);
-                        _logger.Info($"Client: {ClientID} Version: {packet.Client.Version}");
-                        var resp = new VersionCheckResponse(req.Major, req.Minor, req.Version);
-                        response = resp.ToBytes();
-                        break;
-                    }
+                    var req = VersionCheckRequest.FromBytes(payload);
+                    _logger.Info($"Client: {ClientID} Version: {packet.Client.Version}");
+                    var resp = new VersionCheckResponse(req.Major, req.Minor, req.Version);
+                    _ = Client.SendAsync(PacketType.VersionCheckResponse, resp.ToBytes());
+                    break;
                 case PacketType.AuthenticateRequest:
-                    {
-                        var req = AuthenticateRequest.FromBytes(payload);
-                        _logger.Info($"Username: '{req.Username}', Password: {req.Password}");
-                        //bool valid = await repo.ValidateCredentialsAsync(req.Username, req.Password);
-                        uint userID = 31874;
-                        var resp = new AuthenticateResponse(userID);
-                        response = resp.ToBytes();
-                        
-                        break;
-                    }
+                    var AuthReq = AuthenticateRequest.FromBytes(payload);
+                    _logger.Info($"Username: '{AuthReq.Username}', Password: {AuthReq.Password}");
+                    //bool valid = await repo.ValidateCredentialsAsync(req.Username, req.Password);
+                    uint userID = 31874;
+                    var AuthResp = new AuthenticateResponse(userID);
+                    _ = Client.SendAsync(PacketType.AuthenticateResponse, AuthResp.ToBytes());
+                    break;
                 case PacketType.WorldListRequest:
+                    //Get Worlds from Repo
+                    var WorldList = new List<WorldEntry>
                     {
-                        var WorldList = new List<WorldEntry>
-                        {
-                            new(0, "test", "test2"),
-                        };
-                        var worldListResponse = new WorldListResponse(WorldList);
-                        response = worldListResponse.ToBytes();
-                        break;
-                    }
+                        new(0, "test", "test2")
+                    };
+                    var worldListResponse = new WorldListResponse(WorldList);
+                    _ = Client.SendAsync(PacketType.WorldListResponse, worldListResponse.ToBytes());
+                    break;
                 case PacketType.WorldSelectRequest:
-                    {
-                        var WorldSelectReq = WorldSelectRequest.FromBytes(payload);
-                        var selectedWorldID = WorldSelectReq.WorldID;
-                        _logger.Info($"World Selected: {selectedWorldID}");
-                        var WorldSelectResp = new WorldSelectResponse("127.0.0.1", 50052);
-                        response = WorldSelectResp.ToBytes();
-                        _logger.Info($"Sending Server details for {Client.Id}");
-                        break;
-                    }
+                    //Get World from Repo by ID
+                    var WorldSelectReq = WorldSelectRequest.FromBytes(payload);
+                    var selectedWorldID = WorldSelectReq.WorldID;
+                    _logger.Info($"World Selected: {selectedWorldID}");
+                    var WorldSelectResp = new WorldSelectResponse("127.0.0.1", 50052);
+                    _ = Client.SendAsync(PacketType.WorldSelectResponse, WorldSelectResp.ToBytes());
+                    break;
                 case PacketType.PingRequest:
-                    {
-                        var ping = PingRequest.FromBytes(payload);
-                        response = ping.ToBytes();
-                        break;
-                    }
+                    var ping = PingRequest.FromBytes(payload);
+                    _ = Client.SendAsync(PacketType.PingResponse, ping.ToBytes());
+                    break;
                 default:
-                    {
-                        _logger.Error($"Unknown packet type: {packet.Type:X4}");
-                        break;
-                    }
+                    _logger.Error($"Unknown packet type: {packet.Type:X4}");
+                    break;
             }
-            _ = Client.SendAsync(response);
-
-
-
         }
-        
     }
 }
