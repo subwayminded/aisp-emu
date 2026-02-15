4c928a04c7dbfed33deb24e63a5d2496e56e48f2
Tricon2Elf <158119828+tricon2elf@users.noreply.github.com>
2026-02-15 14:34:35 +1300

    Rename Id to ItemId in ItemSlotInfo

--- AISpace.Common/Game/ItemSlotInfo.cs
+++ AISpace.Common/Game/ItemSlotInfo.cs
@@ -2,13 +2,13 @@
 
 public class ItemSlotInfo(uint id, uint socket)
 {
-    public uint Id = id;//4
+    public uint ItemId = id;//4
     public uint Socket = socket;//4
 
     public byte[] ToBytes()
     {
         var writer = new Network.PacketWriter();
-        writer.Write(Id);
+        writer.Write(ItemId);
         writer.Write(Socket);
         return writer.ToBytes();
     }
