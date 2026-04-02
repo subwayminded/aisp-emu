bf9ba398c912f618d4a00783c5e8ca7f44781388
subwayminded <technomaniac25@gmail.com>
2026-04-02 19:26:28 +0300

    Update AreaItemGetListHandler.cs

--- AISpace.Common/Handlers/Area/AreaItemGetListHandler.cs
+++ AISpace.Common/Handlers/Area/AreaItemGetListHandler.cs
@@ -1,20 +1,38 @@
+using AISpace.Common.DAL;
 using AISpace.Common.Game;
 using AISpace.Network;
 using AISpace.Network.Packets.Area;
+using Microsoft.EntityFrameworkCore;
 
 namespace AISpace.Common.Handlers.Area;
 
-public class AreaItemGetListHandler : IPacketHandler
+public class AreaItemGetListHandler(MainContext db) : IPacketHandler
 {
     public PacketType RequestType => PacketType.ItemGetListRequest;
-
     public PacketType ResponseType => PacketType.ItemGetListResponse;
-
     public MessageDomain Domain => MessageDomain.Area;
 
     public async Task HandleAsync(ReadOnlyMemory<byte> payload, IPlayerSession session, CancellationToken ct = default)
     {
         var response = new ItemGetListResponse(0);
         await session.SendAsync(ResponseType, response.ToBytes(), ct);
+
+        var chara = await db.Characters
+            .Include(c => c.Inventory)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Id == session.CharacterId, ct);
+
+
+        if (chara != null && chara.Inventory != null)
+        {
+            uint placeIndex = 0;
+            foreach (var invItem in chara.Inventory)
+            {
+                var notify = new ItemUpdateListNotify(placeIndex, (uint)invItem.ItemId, (uint)invItem.ItemId);
+                await session.SendAsync(PacketType.ItemUpdateListNotify, notify.ToBytes(), ct);
+                
+                placeIndex++;
+            }
+        }
     }
 }
